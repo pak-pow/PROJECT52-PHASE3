6bb96b7ac6c9ce370b4cffcc409de22f0d023539
vince <paulag10987@gmail.com>
2026-09-22 22:07:43 +0800

    feat(app): implement application entrypoint

--- week39_ecommerce_store/backend/run.py
+++ week39_ecommerce_store/backend/run.py
@@ -1,1 +1,8 @@
+from app import create_app
+from app.config.settings import get_config
 
+config = get_config()
+app = create_app(config)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=config.PORT, debug=config.DEBUG)
